68f9ca750de7e4674885b32b151bc586166fd0c8
Luciano Alcaraz <LGA-dev@hotmail.com>
2025-06-09 04:31:58 -0300

    FEAT: Update code format (AStyle)

--- funciones.cpp
+++ funciones.cpp
@@ -111,92 +111,110 @@ void seleccionarNombres(string &jugador1, string &jugador2)
     getline(cin, jugador2);
 }
 
-void interfazJuego(string jugador1, string jugador2, int numeroDeRonda, int dadosJugador1, int dadosJugador2, int puntajeJugador1, int puntajeJugador2,bool turno) {
+void interfazJuego(string jugador1, string jugador2, int numeroDeRonda, int dadosJugador1, int dadosJugador2, int puntajeJugador1, int puntajeJugador2,bool turno)
+{
     system("cls");
     int contadorStock=0,seleccion,sumaStock=0,x,dadosObjetivo[2],dadosStockElegidos[12],dadosStock[12],resultadoObjetivo=0;
     char confirmacion;
-    for (int x=0;x<2;x++){
+    for (int x=0; x<2; x++)
+    {
         dadosObjetivo[x] = tirarDado(12);
         resultadoObjetivo+= dadosObjetivo[x];
     }
 
     cout << "============================================================" << endl;
-    cout << "                       Enfrendados" << endl;
+    cout << "                       Enfrendados 123" << endl;
     cout << "============================================================" << endl << endl;
 
     cout << "Turno de:";
-    if (turno == true){
+    if (turno == true)
+    {
         cout << jugador1;
     }
-    else {
+    else
+    {
         cout << jugador2;
     }
     cout << "                                               Dados objetivo (d12):" << dadosObjetivo[0] << " + " << dadosObjetivo[1] << " = " << resultadoObjetivo << endl;
     cout << "Stock actual: ";
-        if (turno==true){
-                        cout << dadosJugador1 << ": [";
-                for (x=0;x<dadosJugador1;x++){
-                    dadosStock[x] = tirarDado(6);
-                    sumaStock += dadosStock[x];
-                    cout << dadosStock[x];
-                        if (x < dadosJugador1-1){
-                            cout << ",";
-                        }
-                }
-
+    if (turno==true)
+    {
+        cout << dadosJugador1 << ": [";
+        for (x=0; x<dadosJugador1; x++)
+        {
+            dadosStock[x] = tirarDado(6);
+            sumaStock += dadosStock[x];
+            cout << dadosStock[x];
+            if (x < dadosJugador1-1)
+            {
+                cout << ",";
             }
-        else {
-            cout << dadosJugador2 << ": [ ";
-                for (x=0;x<dadosJugador2;x++){
-                    dadosStock[x] = tirarDado(6);
-                    sumaStock += dadosStock[x];
-                    cout << dadosStock[x];
-                        if (x < dadosJugador2-1){
-                            cout << " , ";
-                        }
-                }
+        }
+
+    }
+    else
+    {
+        cout << dadosJugador2 << ": [ ";
+        for (x=0; x<dadosJugador2; x++)
+        {
+            dadosStock[x] = tirarDado(6);
+            sumaStock += dadosStock[x];
+            cout << dadosStock[x];
+            if (x < dadosJugador2-1)
+            {
+                cout << " , ";
             }
-        cout << "]" << endl;
+        }
+    }
+    cout << "]" << endl;
 
-            if(sumaStock<resultadoObjetivo){
-                cout << "No se puede llegar al numero objetivo, turno perdido" << endl;
+    if(sumaStock<resultadoObjetivo)
+    {
+        cout << "No se puede llegar al numero objetivo, turno perdido" << endl;
+    }
+    else
+    {
+        sumaStock=0;
+        cout << "Seleccionar dados: ";
+        while(resultadoObjetivo > sumaStock)
+        {
+
+            cin >> seleccion;
+            while (dadosStock[seleccion-1]==0)
+            {
+                cout << "Dado incorrecto, por favor seleccionar otro: ";
+                cin >> seleccion;
             }
-            else {
-                    sumaStock=0;
-                cout << "Seleccionar dados: ";
-                    while(resultadoObjetivo > sumaStock){
+            dadosStockElegidos[contadorStock] = dadosStock[seleccion-1];
+            dadosStock[seleccion-1] = 0;
+            sumaStock += dadosStockElegidos[contadorStock];
+            contadorStock++;
+        }
 
-                        cin >> seleccion;
-                            while (dadosStock[seleccion-1]==0){
-                                cout << "Dado incorrecto, por favor seleccionar otro: ";
-                                cin >> seleccion;
-                            }
-                        dadosStockElegidos[contadorStock] = dadosStock[seleccion-1];
-                        dadosStock[seleccion-1] = 0;
-                        sumaStock += dadosStockElegidos[contadorStock];
-                        contadorStock++;
-                }
-
-                for (x=0;x<contadorStock;x++){
-                    cout << dadosStockElegidos[x];
-                        if (x< contadorStock-1){
-                            cout << " + ";
-                        }
-                }
-                    if (resultadoObjetivo < sumaStock){
-                            cout << " = " << sumaStock << endl;
-                        cout << "El numero sumado es mayor al objetivo";
-                        sumaStock=0;
-                    }
-                    else {
-                        cout << " = " << sumaStock;
-                    }
-
-                cout << endl << "Puntos: 12 x 3 = 36" << endl;
-                cout << "Transfiere 3 dados a Maria" << endl;
-                cout << jugador1 << "3 dados restantes, 36 pts" << endl;
-                cout << jugador2 << "9 dados (recibio 3)" << endl;
+        for (x=0; x<contadorStock; x++)
+        {
+            cout << dadosStockElegidos[x];
+            if (x< contadorStock-1)
+            {
+                cout << " + ";
             }
+        }
+        if (resultadoObjetivo < sumaStock)
+        {
+            cout << " = " << sumaStock << endl;
+            cout << "El numero sumado es mayor al objetivo";
+            sumaStock=0;
+        }
+        else
+        {
+            cout << " = " << sumaStock;
+        }
+
+        cout << endl << "Puntos: 12 x 3 = 36" << endl;
+        cout << "Transfiere 3 dados a Maria" << endl;
+        cout << jugador1 << "3 dados restantes, 36 pts" << endl;
+        cout << jugador2 << "9 dados (recibio 3)" << endl;
+    }
 }
 
 int tirarDado(int caras)
@@ -204,23 +222,26 @@ int tirarDado(int caras)
     return rand() % caras + 1;
 }
 
-bool quienEmpieza(bool turno){
-        int j1,j2;
-            do
-                {
+bool quienEmpieza(bool turno)
+{
+    int j1,j2;
+    do
+    {
         j1 = tirarDado(6);
         j2 = tirarDado(6);
-       // cout << j1 << "   " << j2 << endl;
-            }
-            while(j1 == j2);
-            //system("pause");
-            if (j1 > j2){
-                return true;
-            }
-            else {
-                return false;
-            }
-
-
-
+        // cout << j1 << "   " << j2 << endl;
     }
+    while(j1 == j2);
+    //system("pause");
+    if (j1 > j2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
+
+}
